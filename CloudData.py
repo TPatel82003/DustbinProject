07a93b2021f3c714d41646a540852b9981b4ba90
Tirth Patel <86900792+TENDAMFELIX@users.noreply.github.com>
2023-01-05 12:03:12 +0530

    Add files via upload

---
+++ CloudData.py
@@ -0,0 +1,20 @@
+import urllib.request
+import requests
+import threading
+import json
+
+import random
+
+def thingspeak_post():
+    threading.Timer(15,thingspeak_post).start()
+    val=random.randint(1,30)
+    URl='https://api.thingspeak.com/update?api_key='
+    KEY='QKBAEIL8OGG0O5N8'
+    HEADER='&field1={}'.format(val)
+    NEW_URL = URl+KEY+HEADER
+    print(NEW_URL)
+    data=urllib.request.urlopen(NEW_URL)
+    print(data)
+    
+if __name__ == '__main__':
+    thingspeak_post()
---
+++ Distance.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+
+TRIG = 23
+ECHO = 24
+GPIO.setmode(GPIO.BCM)
+import urllib.request
+
+import random
+
+
+def thingspeak_post(Distance):
+    URl = "https://api.thingspeak.com/update?api_key="
+    KEY = "QKBAEIL8OGG0O5N8"
+    HEADER = "&field1={}".format(Distance)
+    NEW_URL = URl + KEY + HEADER
+    print(NEW_URL)
+    data = urllib.request.urlopen(NEW_URL)
+    print(data)
+
+i = 1
+while i <= 1:
+    print("distance measurement in progress")
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+    GPIO.output(TRIG, False)
+    print("waiting for sensor to settle")
+    time.sleep(0.2)
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+    print("distance:", distance, "cm")
+    thingspeak_post(distance)
+    time.sleep(2)
+    i = i + 1
